6d1b56e98ad0f1d2e54a49f3c62f1fb86a2b4abc
avd151 <deshpandeav19.comp@coep.ac.in>
2021-11-10 18:57:48 +0530

    Added basic example code to write testcases for registration form

---
+++ test_register.py
@@ -0,0 +1,11 @@
+import unittest
+import register
+
+class TestRegister(unittest.TestCase):
+    
+    #Add testcases in functions starting with test_funcname() to test different fields in registration form for student and faculty
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
