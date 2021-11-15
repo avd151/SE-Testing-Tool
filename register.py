1b12216abbc2dfcd18cf1a43a831ab9981a2333b
avd151 <deshpandeav19.comp@coep.ac.in>
2021-11-15 11:34:40 +0530

    validation for student details, and display error or warning messages accordingly

--- register.py
+++ register.py
@@ -1,5 +1,7 @@
 from tkinter import *
 import mysql.connector
+from tkinter import messagebox
+
 
 mysqldb=mysql.connector.connect(host="localhost",user="root",password="",database="se_cms")
 mycursor=mysqldb.cursor()
@@ -15,6 +17,82 @@ def create_entry(screen_name, text_field, width_field, height_field, font_name, 
     Label(screen_name, text='').pack()
     return field_name_entry
 
+def validate_email(email_addr):
+    if '@coep.ac.in' in email_addr:
+        return True
+    else:
+        return False
+
+def validate_mobileno(mobile_no):
+    if len(mobile_no) == 10:
+        return True
+    else:
+        return False
+
+def validate_dob(dob):
+    if len(dob) == 10:
+        if (int(dob[6:]) <= 2003):
+            print(int(dob[6:]))
+            return True
+        else:
+            return False
+    else:
+        return False
+
+def validate_misno(misno):
+    if len(misno) == 9:
+        return True
+    else:
+        return False
+
+def popup_msg():
+    mis_flag = False
+    mobile_flag = False
+    email_flag = False
+    dob_flag = False
+    student_name_info = student_name.get()
+    student_misno_info = student_misno.get()
+    student_mobileno_info = student_mobileno.get()
+    student_emailaddr_info = student_emailaddr.get()
+    student_year_info = student_year.get()
+    student_branch_info = student_branch.get()
+    student_dob_info = student_dob.get()
+
+    #Validate mis no.
+    if (validate_misno(student_misno_info) == False):
+        messagebox.showerror('Invalid MIS Number',
+                             'Please enter a valid MIS No. e.g. 111903010')
+        return
+    else:
+        mis_flag = True
+
+    #Validate mobile number
+    if (validate_mobileno(student_mobileno_info) == False):
+        messagebox.showerror('Invalid Mobile Number',
+                             'Please enter a valid Mobile Number e.g. 9420456891')
+        return
+    else:
+        mobile_flag = True
+
+    #Valid email address
+    if (validate_email(student_emailaddr_info) == False):
+        messagebox.showwarning('Invalid Email Address',
+                             'Please enter a valid college email address e.g. deshpandeav19.comp@coep.ac.in')
+        return
+    else:
+        email_flag = True
+        
+    #Validate DOB
+    if (validate_dob(student_dob_info) == False):
+        messagebox.showerror('Invalid Date of Birth',
+                             'Please enter correct date of birth details. If they are still correct, you are below 18 years of age, hence not eligible to join the college.')
+        return
+    else:
+        dob_flag = True
+
+    if (mobile_flag and email_flag and dob_flag and mis_flag):
+        student_register_util()
+
 #Function to store student details in a file
 def student_register_util():
     student_name_info = student_name.get()
@@ -24,28 +102,30 @@ def student_register_util():
     student_year_info = student_year.get()
     student_branch_info = student_branch.get()
     student_dob_info = student_dob.get()
-    student_aadharno_info = student_aadharno.get()
-    fs = open('student_info.txt', 'a')
-    fs.write(student_fields[0] + ' - ' + student_name_info + '\n')
-    fs.write(student_fields[1] + ' - ' + student_misno_info + '\n')
-    fs.write(student_fields[2] + ' - ' + student_mobileno_info + '\n')
-    fs.write(student_fields[3] + ' - ' + student_emailaddr_info + '\n')
-    fs.write(student_fields[4] + ' - ' + student_year_info + '\n')
-    fs.write(student_fields[5] + ' - ' + student_branch_info + '\n')
-    fs.write(student_fields[6] + ' - ' + student_dob_info + '\n')
-    fs.write(student_fields[7] + ' - ' + student_aadharno_info + '\n')
-    fs.write('\n')
+    
+    #popup()
+    # fs = open('student_info.txt', 'a')
+    # fs.write(student_fields[0] + ' - ' + student_name_info + '\n')
+    # fs.write(student_fields[1] + ' - ' + student_misno_info + '\n')
+    # fs.write(student_fields[2] + ' - ' + student_mobileno_info + '\n')
+    # fs.write(student_fields[3] + ' - ' + student_emailaddr_info + '\n')
+    # fs.write(student_fields[4] + ' - ' + student_year_info + '\n')
+    # fs.write(student_fields[5] + ' - ' + student_branch_info + '\n')
+    # fs.write(student_fields[6] + ' - ' + student_dob_info + '\n')
+    # fs.write(student_fields[7] + ' - ' + student_aadharno_info + '\n')
+    # fs.write('\n')
     Label(student_register_screen, text='').pack()
     Label(student_register_screen, text="Student Registration Successful!", width='300',
           height='2', font=('Times New Roman', 20), fg='green').pack()
     try:
         #student dob is doubtful
-        sql = "INSERT INTO  student (stud_misno, stud_name, stud_mobileno, stud_year) VALUES (%s, %s, %s, %s)"
-        val = (student_misno_info, student_name_info, student_mobileno_info, student_year_info)
+        #sql = "INSERT INTO  student (stud_misno, stud_name, stud_mobileno, stud_year) VALUES (%s, %s, %s, %s)"
+        sql = "INSERT INTO student_register (student_name, student_misno, student_mobileno, student_emailaddr, student_year, student_branch, student_dob) VALUES(%s, %s, %s, %s, %s, %s, %s)"
+        val = (student_name_info, student_misno_info, student_mobileno_info, student_emailaddr_info, student_year_info, student_branch_info, student_dob_info)
         mycursor.execute(sql, val)
         mysqldb.commit()
         lastid = mycursor.lastrowid
-        print("Saving successful")
+        print("Student data saved successfully")
         # messagebox.showinfo("information", "Student Record Saved Successfully")
     #    e1.delete(0, END)
     #    e2.delete(0, END)
@@ -60,7 +140,7 @@ def student_register_util():
 def student_register():
     # 'Admission Date' 'Secondary Email Address' remains doubtful to add or not
     global student_fields 
-    student_fields = ['Full Name', 'MIS Number', 'Mobile Number', 'Email Address', 'Year of Study', 'Branch', 'Date of Birth', 'Aadhar Number']
+    student_fields = ['Full Name', 'MIS Number', 'Mobile Number', 'Email Address', 'Year of Study', 'Branch', 'Date of Birth(DD-MM-YYYY)']
     global student_register_screen
     student_register_screen = Toplevel(screen)
     student_register_screen.title('Student Registration Form')
@@ -73,7 +153,6 @@ def student_register():
     global student_year
     global student_branch
     global student_dob
-    global student_aadharno
     # student_name, student_misno, student_mobileno, student_emailaddr, student_year, student_branch, student_dob, student_aadharno = StringVar()
     student_name = StringVar()
     student_misno = StringVar()
@@ -82,7 +161,6 @@ def student_register():
     student_year = StringVar()
     student_branch = StringVar()
     student_dob = StringVar()
-    student_aadharno = StringVar()
     #Label
     Label(student_register_screen, text='Please Enter Details below', width='300',
           height='2', bg='grey', font=('Times New Roman', 15)).pack()
@@ -113,13 +191,9 @@ def student_register():
     student_dob_entry = create_entry(
         student_register_screen, student_fields[6] + ' *', '300', '2', 'Times New Roman', 12, student_dob, 100)
     print(student_dob)
-    # #Student aadhar no.
-    student_aadharno_entry = create_entry(
-        student_register_screen, student_fields[7] + ' *', '300', '2', 'Times New Roman', 12, student_aadharno, 100)
-    print(student_aadharno)
     #Submit button
     Button(student_register_screen, text='Submit', height='2', width='10', bg='green',
-           font=('Times New Roman', 14), command=student_register_util).pack()
+           font=('Times New Roman', 14), command=popup_msg).pack()
 
 #Stores faculty details
 def faculty_register_util():
